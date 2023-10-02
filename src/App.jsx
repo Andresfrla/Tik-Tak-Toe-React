c2c8692efdd1ff4572e2a5dbb4e94ab37b3099be
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-10-01 19:29:15 -0600

    adjusting the winning

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const TURNS = {
@@ -25,19 +23,57 @@ const Square = ({ children, isSelected ,updateBoard, i}) => {
   )
 } 
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
     )
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if( 
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[b] === boardToCheck[c]
+      ) {
+        return boardToCheck[a] 
+      }
+    }
+  }
 
   const updateBoard = (i) => {
+    // si tiene algo 
+    if(board[i] || winner) return
+
+    // actualizar el tablero
     const newBoard = [...board]
+    //siempre se tiene que copiar el arreglo para no mutar el estado original
     newBoard[i] = turn
     setBoard(newBoard)
 
+    //Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //revisar un ganador
+
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
   }
 
   return (
